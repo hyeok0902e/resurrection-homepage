173b3181e4d1bce90b2a633b9f4b76cbb08386b7
전준혁 <hyeoke@jeonjunhyeog-ui-MacBookPro.local>
2019-09-17 22:32:35 +0900

    remove config in .gitignore

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: "newlife4829",
+    database: 'book1',
+    port: 3306,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: 'false',
+  },
+  production: {
+    username: 'root',
+    password: "newlife4829",
+    database: 'book1',
+    port: 3306,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: 'false',
+    logging: false,
+  },
+};
+

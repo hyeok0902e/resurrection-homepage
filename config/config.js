ffd5fd5e7a52d2213496dbc6c967bb40d4e34c16
전준혁 <hyeoke@jeonjunhyeog-ui-MacBookPro.local>
2019-09-17 23:18:33 +0900

    config/config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'root',
-    password: "newlife4829",
+    password: process.env.SEQUELIZE_DEV_PW,
     database: 'book1',
     port: 3306,
     host: '127.0.0.1',
@@ -13,6 +13,7 @@ module.exports = {
   production: {
     username: 'root',
     database: 'resurrection-homepage',
+    password: process.env.SEQUELIZE_PRO_PW,
     port: 3306,
     host: '127.0.0.1',
     dialect: 'mysql',
